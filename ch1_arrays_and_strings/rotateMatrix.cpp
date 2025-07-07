7e37917ddeb00c1050692d0a1856874f0977fc23
Ben <rogersben219@gmail.com>
2025-07-07 10:18:07 -0400

    rotateMatrix.cpp w/ matrix generator

---
+++ ch1_arrays_and_strings/rotateMatrix.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+// rotate matrix by 90 degrees
+int* rotateMatrix(int matrix[]) {
+
+}
+
+int main() {
+    // Generate 10x10 byte array of ints (4bytes)
+    int byteArray[10][10];
+    for (int i=0; i<10; i++) {
+        for (int j=0; j<10; j++) {
+            byteArray[i][j] = i * 10 + j;
+        }
+    }
+    // for (int i=0; i<10; i++) {
+    //     for (int j=0; j<10; j++) {
+    //         std::cout << byteArray[i][j];
+    //     }
+    // }
+}
